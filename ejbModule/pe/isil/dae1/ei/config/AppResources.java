faa20c51b0fccbca911162ccefdc9570fb91573a
Jasas-LaptopHp <jasasrobles02@gmail.com>
2025-12-01 19:56:07 -0500

    Agregando AppResources global

---
+++ ejbModule/pe/isil/dae1/ei/config/AppResources.java
@@ -0,0 +1,23 @@
+package pe.isil.dae1.ei.config;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.ejb.Singleton;
+import jakarta.ejb.Startup;
+
+@Singleton
+@Startup
+@DataSourceDefinition(
+		name="java:app/jdbc/EI_DAE1DS",
+		className="com.microsoft.sqlserver.jdbc.SQLServerDriver",
+		url="jdbc:sqlserver://localhost:1433;databaseName=DAE1_EI;encrypt=true;trustServerCertificate=true",
+		user="",
+		//Colocar el usuario de login para conectarte a sql server
+		password=""
+		//colocar la password que usas para conectarte a sql server
+)
+
+
+
+public class AppResources {
+
+}
